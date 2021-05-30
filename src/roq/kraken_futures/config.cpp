cc7b782ba0070f4ab4d2a97d3e298ffcfd3dacbf
thraneh <thraneh@gmail.com>
2021-05-30 06:37:11 +0200

    update GatewaySettings layout

--- src/roq/kraken_futures/config.cpp
+++ src/roq/kraken_futures/config.cpp
@@ -53,10 +53,6 @@ void Config::dispatch(server::Config::Handler &handler) const {
   for (auto &user : users)
     handler(user);
   server::Settings settings{
-      .mbp_max_depth = Flags::ws_public_subscribe_book_depth(),
-      .mbp_allow_price_inversion = {},
-      .mbp_allow_fractional_tick_size = {},
-      .mbp_allow_remove_non_existing = {},
       .supports{
           SupportType::REFERENCE_DATA,
           SupportType::MARKET_STATUS,
@@ -67,6 +63,10 @@ void Config::dispatch(server::Config::Handler &handler) const {
           SupportType::CANCEL_ORDER,
           SupportType::ORDER_ACK,
       },
+      .mbp_max_depth = Flags::ws_public_subscribe_book_depth(),
+      .mbp_allow_price_inversion = {},
+      .mbp_allow_fractional_tick_size = {},
+      .mbp_allow_remove_non_existing = {},
   };
   handler(settings);
 }
