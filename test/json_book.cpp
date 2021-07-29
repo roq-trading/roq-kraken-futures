82776fa5e1afb5937b7afbec8eb53d4c81e54f47
thraneh <thraneh@gmail.com>
2021-07-29 07:25:25 +0200

    tests

---
+++ test/json_book.cpp
@@ -0,0 +1,32 @@
+/* Copyright (c) 2017-2021, Hans Erik Thrane */
+
+#include <gtest/gtest.h>
+
+#include "roq/core/json/parser.h"
+
+#include "roq/kraken_futures/json/book.h"
+
+using namespace roq;
+using namespace roq::kraken_futures;
+
+using namespace std::chrono_literals;
+
+TEST(json_book, simple) {
+  auto message = R"({)"
+                 R"("feed":"book",)"
+                 R"("product_id":"PI_XBTUSD",)"
+                 R"("side":"buy",)"
+                 R"("seq":1766422,)"
+                 R"("price":39826.0,)"
+                 R"("qty":3951.0,)"
+                 R"("timestamp":1627535639684)"
+                 R"(})";
+  auto obj = core::json::Parser::create<json::Book>(message);
+  EXPECT_EQ(obj.feed, json::Feed::BOOK);
+  EXPECT_EQ(obj.product_id, "PI_XBTUSD"_sv);
+  EXPECT_EQ(obj.side, json::Side::BUY);
+  EXPECT_EQ(obj.seq, 1766422);
+  EXPECT_DOUBLE_EQ(obj.price, 39826.0);
+  EXPECT_DOUBLE_EQ(obj.qty, 3951.0);
+  EXPECT_EQ(obj.timestamp, 1627535639684ms);
+}
---
+++ test/json_book_snapshot.cpp
@@ -0,0 +1,49 @@
+/* Copyright (c) 2017-2021, Hans Erik Thrane */
+
+#include <gtest/gtest.h>
+
+#include "roq/core/buffer.h"
+
+#include "roq/core/json/buffer.h"
+#include "roq/core/json/parser.h"
+
+#include "roq/kraken_futures/json/book_snapshot.h"
+
+using namespace roq;
+using namespace roq::kraken_futures;
+
+using namespace std::chrono_literals;
+
+TEST(json_book_snapshot, simple) {
+  auto message = R"({)"
+                 R"("feed":"book_snapshot",)"
+                 R"("product_id":"FI_LTCUSD_210924",)"
+                 R"("timestamp":1627535620727,)"
+                 R"("seq":732887,)"
+                 R"("tickSize":null,)"
+                 R"("bids":[)"
+                 R"({"price":139.54,"qty":5000.0},)"
+                 R"({"price":139.53,"qty":5000.0},)"
+                 R"({"price":139.52,"qty":5000.0},)"
+                 R"({"price":139.51,"qty":5000.0},)"
+                 R"({"price":139.5,"qty":5000.0})"
+                 R"(],)"
+                 R"("asks":[)"
+                 R"({"price":139.61,"qty":5000.0},)"
+                 R"({"price":139.62,"qty":5000.0},)"
+                 R"({"price":139.63,"qty":5000.0},)"
+                 R"({"price":139.64,"qty":5000.0},)"
+                 R"({"price":139.65,"qty":5000.0})"
+                 R"(])"
+                 R"(})";
+  core::Buffer buffer(8192);
+  core::json::Buffer buffer_(buffer);
+  auto obj = core::json::Parser::create<json::BookSnapshot>(message, buffer_);
+  EXPECT_EQ(obj.feed, json::Feed::BOOK_SNAPSHOT);
+  EXPECT_EQ(obj.product_id, "FI_LTCUSD_210924"_sv);
+  EXPECT_EQ(obj.timestamp, 1627535620727ms);
+  EXPECT_EQ(obj.seq, 732887);
+  // tick_size
+  EXPECT_EQ(std::size(obj.bids), 5);
+  EXPECT_EQ(std::size(obj.asks), 5);
+}
