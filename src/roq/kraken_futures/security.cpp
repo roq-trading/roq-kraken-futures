f552c0d6d21a18f42d7092acfaee4e146cd68e55
thraneh <thraneh@gmail.com>
2021-08-04 14:29:20 +0200

    add option to opt-out of nonce

--- src/roq/kraken_futures/security.cpp
+++ src/roq/kraken_futures/security.cpp
@@ -16,6 +16,8 @@
 
 #include "roq/core/crypto/sha.h"
 
+#include "roq/kraken_futures/flags.h"
+
 using namespace std::literals;
 using namespace roq::literals;
 
@@ -51,31 +53,55 @@ Security::Security(
 
 std::string Security::create_headers(const std::string_view &path, const std::string_view &query) {
   assert(!path.empty());
-  auto nonce = roq::format("{}"_sv, get_nonce().count());
-  sha_.clear();
-  if (!query.empty()) {
-    assert(query[0] == '?');
-    auto raw = query.substr(1);  // note! not including '?'
-    sha_.update(raw);
+  if (Flags::rest_use_nonce()) {
+    auto nonce = roq::format("{}"_sv, get_nonce().count());
+    sha_.clear();
+    if (!query.empty()) {
+      assert(query[0] == '?');
+      auto raw = query.substr(1);  // note! not including '?'
+      sha_.update(raw);
+    }
+    sha_.update(nonce);
+    sha_.update(path);
+    std::array<char, 32> buffer_1;
+    auto length_1 = sha_.digest(buffer_1);
+    assert(length_1 == buffer_1.size());
+    hmac_.clear();
+    hmac_.update(buffer_1.data(), buffer_1.size());
+    std::array<char, 64> buffer_2;
+    auto length_2 = hmac_.digest(buffer_2);
+    assert(length_2 == buffer_2.size());
+    auto authent = core::binascii::Base64::encode(buffer_2);
+    return roq::format(
+        "APIKey: {}\r\n"
+        "Nonce: {}\r\n"
+        "Authent: {}\r\n"_sv,
+        key_,
+        nonce,
+        authent);
+  } else {
+    sha_.clear();
+    if (!query.empty()) {
+      assert(query[0] == '?');
+      auto raw = query.substr(1);  // note! not including '?'
+      sha_.update(raw);
+    }
+    sha_.update(path);
+    std::array<char, 32> buffer_1;
+    auto length_1 = sha_.digest(buffer_1);
+    assert(length_1 == buffer_1.size());
+    hmac_.clear();
+    hmac_.update(buffer_1.data(), buffer_1.size());
+    std::array<char, 64> buffer_2;
+    auto length_2 = hmac_.digest(buffer_2);
+    assert(length_2 == buffer_2.size());
+    auto authent = core::binascii::Base64::encode(buffer_2);
+    return roq::format(
+        "APIKey: {}\r\n"
+        "Authent: {}\r\n"_sv,
+        key_,
+        authent);
   }
-  sha_.update(nonce);
-  sha_.update(path);
-  std::array<char, 32> buffer_1;
-  auto length_1 = sha_.digest(buffer_1);
-  assert(length_1 == buffer_1.size());
-  hmac_.clear();
-  hmac_.update(buffer_1.data(), buffer_1.size());
-  std::array<char, 64> buffer_2;
-  auto length_2 = hmac_.digest(buffer_2);
-  assert(length_2 == buffer_2.size());
-  auto authent = core::binascii::Base64::encode(buffer_2);
-  return roq::format(
-      "APIKey: {}\r\n"
-      "Nonce: {}\r\n"
-      "Authent: {}\r\n"_sv,
-      key_,
-      nonce,
-      authent);
 }
 
 std::string Security::signed_challenge(const std::string_view &original_challenge) {
