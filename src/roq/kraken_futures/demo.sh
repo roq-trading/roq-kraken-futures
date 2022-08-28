b9f091c41f323aa6727748101ffd15585a3852c9
thraneh <thraneh@gmail.com>
2022-08-28 18:17:01 +0200

    push

--- src/roq/kraken_futures/demo.sh
+++ src/roq/kraken_futures/demo.sh
@@ -22,9 +22,9 @@ WS_URI="wss://${ENV}$URI/ws/v3"
 $PREFIX ./roq-kraken-futures \
 	--name "$NAME" \
 	--config_file "$CONFIG_FILE" \
-  --event_log_dir "$HOME/var/lib/roq/data" \                                                                            
-  --event_log_symlink \                                                                                                 
-  --client_listen_address "$HOME/run/$NAME.sock" \                                                                      
+  --event_log_dir "$HOME/var/lib/roq/data" \
+  --event_log_symlink \
+  --client_listen_address "$HOME/run/$NAME.sock" \
   --metrics_listen_address "$HOME/run/${NAME}_metrics.sock" \
 	--rest_uri "$REST_URI" \
 	--ws_uri "$WS_URI" \
