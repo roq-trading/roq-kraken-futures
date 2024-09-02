074db6aee99e0c87a9557190cd1c4765cb9f5f27
thraneh <thraneh@gmail.com>
2024-09-02 17:57:30 +0200

    move more charconv to roq-utils

--- src/roq/kraken_futures/json/utils.hpp
+++ src/roq/kraken_futures/json/utils.hpp
@@ -6,9 +6,9 @@
 
 #include "roq/utils/patterns.hpp"
 
-#include "roq/core/json/parser.hpp"
+#include "roq/utils/charconv/from_chars.hpp"
 
-#include "roq/core/charconv/datetime.hpp"
+#include "roq/core/json/parser.hpp"
 
 namespace roq {
 namespace kraken_futures {
@@ -27,7 +27,9 @@ inline void update(std::chrono::milliseconds &result, core::json::Value const &v
           [](bool) { throw std::bad_cast{}; },
           [&](int64_t value) { result = std::chrono::milliseconds{static_cast<uint64_t>(value)}; },
           [&](double value) { result = std::chrono::milliseconds{static_cast<uint64_t>(value)}; },
-          [&](std::string_view const &value) { result = core::charconv::datetime_from_string<std::remove_reference<decltype(result)>::type>(value); },
+          [&](std::string_view const &value) {
+            result = utils::charconv::from_chars<std::remove_reference<decltype(result)>::type>(value, utils::charconv::Format::DATETIME);
+          },
           [](core::json::Object const &) { throw std::bad_cast{}; },
           [](core::json::Array const &) { throw std::bad_cast{}; },
       },
