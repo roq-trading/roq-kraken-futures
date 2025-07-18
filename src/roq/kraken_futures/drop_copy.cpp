bc03126a0dce29f3bd99c8b816adc1a8ea63cf24
thraneh <thraneh@gmail.com>
2025-07-18 16:25:03 +0200

    Better support for estimating P&L and funds (#514)

--- src/roq/kraken_futures/drop_copy.cpp
+++ src/roq/kraken_futures/drop_copy.cpp
@@ -434,9 +434,11 @@ void DropCopy::operator()(Trace<json::FillsSnapshot> const &event) {
           .quantity = item.qty,
           .price = item.price,
           .liquidity = map(item.fill_type),
-          .quote_quantity = NaN,
-          .commission_quantity = item.fee_paid,
+          .base_amount = NaN,
+          .quote_amount = NaN,
+          .commission_amount = item.fee_paid,
           .commission_currency = item.fee_currency,
+          .profit_loss_amount = NaN,
       };
       auto trade_update = TradeUpdate{
           .stream_id = stream_id_,
@@ -581,9 +583,11 @@ void DropCopy::operator()(Trace<json::Fills> const &event) {
           .quantity = item.qty,
           .price = item.price,
           .liquidity = map(item.fill_type),
-          .quote_quantity = NaN,
-          .commission_quantity = item.fee_paid,
+          .base_amount = NaN,
+          .quote_amount = NaN,
+          .commission_amount = item.fee_paid,
           .commission_currency = item.fee_currency,
+          .profit_loss_amount = NaN,
       };
       shared_.fills.emplace_back(std::move(fill));
     }
