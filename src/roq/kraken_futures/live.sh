0c08e01a08817fd6ca870d83579c409da59b6c4d
thraneh <thraneh@gmail.com>
2025-09-25 05:06:00 +0200

    update launch scripts

--- src/roq/kraken_futures/live.sh
+++ src/roq/kraken_futures/live.sh
@@ -12,23 +12,16 @@ CONFIG="${CONFIG:-$NAME}"
 
 CONFIG_FILE="$ROQ_CONFIG_PATH/roq-kraken-futures/$CONFIG.toml"
 
-ENV=""
-
-URI="futures.kraken.com"
-
-REST_URI="https://${ENV}$URI"
-WS_URI="wss://${ENV}$URI/ws/v3"
+FLAGFILE="../../../share/flags/prod/flags.cfg"
 
 $PREFIX ./roq-kraken-futures \
   --name "$NAME" \
   --config_file "$CONFIG_FILE" \
+  --flagfile "$FLAGFILE" \
   --cache_dir "$HOME/var/lib/roq/cache" \
   --event_log_dir "$HOME/var/lib/roq/data" \
-  --event_log_symlink true \
   --client_listen_address "$HOME/run/$NAME.sock" \
   --service_listen_address "$HOME/run/metrics/${NAME}.sock" \
-  --rest_uri "$REST_URI" \
-  --ws_uri "$WS_URI" \
   $@
 
 #  --time_series_interval "60s" \
--- src/roq/kraken_futures/test.sh
+++ src/roq/kraken_futures/test.sh
@@ -35,21 +35,14 @@ CONFIG="${CONFIG:-$NAME-demo}"
 
 CONFIG_FILE="$ROQ_CONFIG_PATH/roq-kraken-futures/$CONFIG.toml"
 
-ENV="demo-"
-
-URI="futures.kraken.com"
-
-REST_URI="https://${ENV}$URI/derivatives"
-WS_URI="wss://${ENV}$URI/ws/v3"
+FLAGFILE="../../../share/flags/test/flags.cfg"
 
 $PREFIX ./roq-kraken-futures \
   --name "$NAME" \
   --config_file "$CONFIG_FILE" \
+  --flagfile "$FLAGFILE" \
   --cache_dir "$HOME/var/lib/roq/cache" \
   --event_log_dir "$HOME/var/lib/roq/data" \
-  --event_log_symlink true \
   --client_listen_address "$HOME/run/$NAME.sock" \
   --service_listen_address "$HOME/run/metrics/${NAME}.sock" \
-  --rest_uri "$REST_URI" \
-  --ws_uri "$WS_URI" \
   $@
